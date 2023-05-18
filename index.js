fe97539286ec277b80d227ab6ef79642b07d416b
Jonathan <jonathan.fetterolf@gmail.com>
2023-05-18 15:26:42 -0400

    functions complete and passing tests

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+
+// should return a new array containing the 
+// first two drivers from the passed-in array
+const returnFirstTwoDrivers = function(drivers) {
+    return [drivers[0], drivers[1]];
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+//       2) should return an array of the last two drivers
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+//       3) has the `returnFirstTwoDrivers` function to as its first element
+//       4) has the `returnLastTwoDrivers` function to as its last element
+//       5) allows us to invoke either function from the array
+
+const createFareMultiplier = function(integer) {
+    return function(fare){
+        return integer * fare
+    }
+}
+//       6) returns a function
+//       7) should multiply a given value using the created multiplier
+
+const fareDoubler = function(fare) {
+    return fare * 2
+}
+//       8) is a function
+//       9) doubles fares
+
+const fareTripler = function(fare) {
+    return fare * 3
+}
+//       10) is a function
+//       11) triples fares
+
+const selectDifferentDrivers = function(arrayOfDrivers, chosenFunction) {
+    return chosenFunction(arrayOfDrivers)
+}
+//       12) returns the first two drivers when passed returnFirstTwoDrivers() as the second argument
+//       13) returns the last two drivers when passed returnLastTwoDrivers() as the second argument
